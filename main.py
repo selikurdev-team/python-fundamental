ced49bf22b090aef90e8a3743a3a00d451d7bbba
selikurdev-team <selikurdev@gmail.com>
2023-08-28 20:30:37 +0700

    add main.py dan pep-on-python.txt file

---
+++ main.py
@@ -0,0 +1,10 @@
+#PEP on Python
+'''yaitu '''
+
+# Sekunsial
+print("Standard Code Python setelah baris kode terakhir ada new line kosong")
+print("Coba bikin code print")
+print('Test Pake single quote')
+print('Test Pake kombinasi quote, "Apakah bisa"')
+
+# Percabangan
